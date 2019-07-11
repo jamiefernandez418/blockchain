49dfe0f25a6d92728208a584c7de6c8a6e946047
gutierrezjdr <gutierrezjdr@gmail.com>
2019-07-10 22:32:52 -0400

    User class with db creation.

--- user.py
+++ user.py
@@ -1,8 +1,37 @@
-class User():
-        def __init__(self,name,id,activate=True):
-            self.name=name
-            self.id=id
-            self.activate=active
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+
+class User(db.model):
+    __tablename__='user'
+
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(80),unique=True,nullable=False)
+    password = db.Column(db.String,nullable=False)
+    email = db.Column(db.String(120),unique=True,nullable=False)
+    authenticated = db.Column(db.Boolean,default = False)
+    
+    def __init__(self,username,email,active=True):
+        self.username=username
+        self.email=email
+        self.active=active
+        
+
+
+    def is_active(self):
+        return self.active
+
+    #def is_authenticated (self):
+        #check if password is same as stored 
+
+    #def get_id(self):
+        #return id of user
+
+    #def is_anonymous(self: (optional)
+        #Returns if user anonymous
+    
 
 
         
