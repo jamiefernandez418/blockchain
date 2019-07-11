bf39e4bec8cf96a27f8a85bba77959ed87c06c35
Jamie Fernandez <jamie.fernandez1021@gmail.com>
2019-07-11 15:02:33 -0400

    Update User.py

--- Willard_Files/User.py
+++ Willard_Files/User.py
@@ -1,11 +1,14 @@
 import random
 
 class User:
+
+	__requestID = {}
+	
     def __init__(self, firstName, lastName):
         self.__firstName = firstName
         self.__lastName = lastName
         self.__randVal = random.randint(1, 123456789)
-        self.__userID = hash(firstName+lastName+str(self.__randVal))
+        self.__userID = hash(firstName+lastName+str(self.__randVal)
     
     def getName(self):
         return (self.__firstName + ' ' + self.__lastName)
@@ -15,3 +18,10 @@ class User:
 
     def __getUserRandVal(self):
         return self.__randVal
+    
+    def __getRequestId(self, book):
+    	return self.__requestId.get(book)
+    #ASSERT: Will return the corresponding request id when given the book as a parameter
+    
+    def __addRequestID(self, book, id):
+    	self.__requestId[book] = id
